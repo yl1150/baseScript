1866c61fa2cda53a0e93a71066c4a05ff0c58e0e
yl1150 <1150484223@qq.com>
2020-07-23 09:43:45 +0800

    添加新的动作

--- Common/ActionMgr.js
+++ Common/ActionMgr.js
@@ -65,7 +65,29 @@ module.exports = {
         if (num) {
             return cc.tween().repeat(num, cc.tween().by(0.1, { position: cc.v2(0, 50) }).by(0.1, { position: cc.v2(0, -50) }))
         } else {
-            return cc.tween().repeatForever(num, cc.tween().by(0.1, { position: cc.v2(0, 50) }).by(0.1, { position: cc.v2(0, -50) }))
+            return cc.tween().repeatForever(cc.tween().by(0.1, { position: cc.v2(0, 50) }).by(0.1, { position: cc.v2(0, -50) }))
+        }
+    },
+
+    jumpAndZoom(num, sx, sy) {
+        if (!sx) {
+            sx = 1;
+        }
+        if (!sy) {
+            sy = sx;
+        }
+        let maxX = sx * 1.2;
+        let minX = sx * 0.8;
+        let midX = (maxX + minX) / 2;
+
+        let maxY = sy * 1.2;
+        let minY = sy * 0.8;
+        let midY = (maxY + minY) / 2;
+        let action = cc.tween().parallel(cc.tween().by(0.1, { position: cc.v2(0, 50) }).by(0.1, { position: cc.v2(0, -50) }),cc.tween().to(0.1, { scaleX: maxX, scaleY: maxY }).to(0.1, { scaleX: midX, scaleY: midY }))
+        if (num) {
+            return cc.tween().repeat(num,action)
+        } else {
+            return cc.tween().repeatForever(action)
         }
     },
 
