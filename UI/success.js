065be761b29ab2f6223838cef6ec32bc734e3eaf
yl1150 <1150484223@qq.com>
2020-08-15 09:45:58 +0800

    添加新的适配方式

--- UI/success.js
+++ UI/success.js
@@ -1,7 +1,19 @@
+const FITLIST = cc.Enum({
+    /**默认展现游戏目录 */
+    default: 0,
+
+    /**psv */
+    psv: 1,
+});
 cc.Class({
     extends: cc.Component,
 
     properties: {
+        fitType: {
+            default: FITLIST.default,
+            type: FITLIST,
+            displayName: '适配方式',
+        }
     },
 
     init() {
@@ -29,8 +41,27 @@ cc.Class({
             var db = array[i].getComponent(dragonBones.ArmatureDisplay)
             db && db.playAnimation("newAnimation", 0)
         }
-        this.caidai.y = -cc.winSize.height / 2 + 180
-        this.caidai2.y = -cc.winSize.height / 2 + 180
+
+        if (this.fitType == FITLIST.psv) {
+            let winSize = cc.view.getFrameSize()
+            let canvas = cc.find('Canvas').getComponent(cc.Canvas);
+            let CanvasSize = canvas.designResolution;
+
+            //web页面DOM的屏幕比例大于16/9时,为全面屏手机
+            if (winSize.width / winSize.height > 16 / 9) {
+                this.caidai.setPosition(this.caidai.position.x, this.caidai.position.y + 150);
+                this.caidai2.setPosition(this.caidai2.position.x, this.caidai2.position.y + 150);
+            }
+            //web页面DOM的屏幕比例小于16/9时,为ipad
+            if (winSize.width / winSize.height <= 16 / 9) {
+                this.caidai.setPosition(this.caidai.position.x + 200, (-winSize.height / 2) / (winSize.width / CanvasSize.width) + 200)
+                this.caidai2.setPosition(this.caidai2.position.x - 200, (-winSize.height / 2) / (winSize.width / CanvasSize.width) + 200)
+            }
+        } else {
+            this.caidai.y = -cc.winSize.height / 2 + 180
+            this.caidai2.y = -cc.winSize.height / 2 + 180
+        }
+
         setTimeout(() => {
             let ddNum = cc.YL.tools.randomNum(1, 3)
             let dd = this.dd.getChildByName('dd' + ddNum)
